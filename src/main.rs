48faba9b5d9cbbc022d9786919bb4d1243f02cb3
Fabio Carpinato <fabiocarpinato@outlook.it>
2024-08-20 11:51:25 +0200

    feat: basic reading and writing over tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fmt::Result,
+    io::{ Read, Write },
+    net::{TcpListener, TcpStream},
+};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream).unwrap();
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+
+    println!("request: {}", request);
+
+    let html = "<div>hello</div>";
+
+    let response = format!("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n\n{}", html.len(), html);
+
+    stream.write(response.as_bytes()).unwrap();
+
+    Ok(())
 }
